f10b47fd137af1ab3ec1873f7c33e4540e26b5a7
Popov Stas <boss.staspopov@mail.ru>
2024-02-27 16:51:17 +0300

    Логика удаления и создания баз данных

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import Depends, FastAPI
+from pydantic import BaseModel
+from contextlib import asynccontextmanager
+from database import create_tables, delete_tables
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await delete_tables()
+    print('delete')
+    await create_tables()
+    print('start')
+    yield
+    print('close')
 
-@app.get('/home')
-def get_home():
-    return {'data': 'Hello world'}
+
+app = FastAPI(lifespan=lifespan)
+
+class STaskAdd(BaseModel):
+    name: str
+    description: str | None = None
+
+class STask(STaskAdd):
+    id: int
+    
+
+tasks = []
+@app.post('/tasks')
+async def add_task(task: Annotated[STaskAdd, Depends()]):
+    tasks.append(task)
+    return {'add': True}
+
+# @app.get('/tasks')
+# def get_home():
+#     task = Task(name='bla bla')
+#     return {'data': task}
 
